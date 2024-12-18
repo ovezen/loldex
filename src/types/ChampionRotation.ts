c39d8edf425400e91c825a96e10b67e6f1c4f117
ovezen <klimen1005@gmail.com>
2024-12-18 13:42:52 +0900

    type

---
+++ src/types/ChampionRotation.ts
@@ -0,0 +1,6 @@
+// 챔피언 로테이션 정보 타입
+export interface ChampionRotation {
+  freeChampionIds: number[];
+  freeChampionIdsForNewPlayers: number[];
+  maxNewPlayerLevel: number;
+}
---
+++ src/types/Item.ts
@@ -0,0 +1,27 @@
+import { Image } from "./Champion";
+
+// 아이템 목록 구조
+export interface ItemList {
+  type: string;
+  version: string;
+  data: {
+    [key: string]: Item;
+  };
+}
+
+// 아이템 기본 정보 타입
+export interface Item {
+  id: string;
+  name: string;
+  description: string;
+  plaintext: string;
+  into: string[];
+  image: Image;
+  gold: {
+    base: number;
+    total: number;
+    sell: number;
+    purchasable: boolean;
+  };
+  tags: string[];
+}
