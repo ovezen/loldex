63b592865d9ccea94082750a81b565970eb4d30a
ovezen <klimen1005@gmail.com>
2024-12-19 06:45:33 +0900

    주석 삭제

--- src/providers/RQProvider.tsx
+++ src/providers/RQProvider.tsx
@@ -1,60 +1,36 @@
-// "use client";
-
-// import {
-//   isServer,
-//   QueryClient,
-//   QueryClientProvider,
-// } from "@tanstack/react-query";
-// import React from "react";
-
-// function makeQueryClient() {
-//   return new QueryClient({
-//     defaultOptions: {
-//       queries: {
-//         staleTime: 60 * 1000,
-//       },
-//     },
-//   });
-// }
-
-// let broswerQueryClient: QueryClient | undefined = undefined;
-
-// function getQueryClient() {
-//   if (isServer) {
-//     return makeQueryClient();
-//   } else {
-//     if (!broswerQueryClient) broswerQueryClient = makeQueryClient();
-//     return broswerQueryClient;
-//   }
-// }
-
-// export default function Providers({ children }: { children: React.ReactNode }) {
-//   const queryClient = getQueryClient();
-//   return (
-//     <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
-//   );
-// }
-
 "use client";
 
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import React, { useState } from "react";
+import {
+  isServer,
+  QueryClient,
+  QueryClientProvider,
+} from "@tanstack/react-query";
+import React from "react";
+
+function makeQueryClient() {
+  return new QueryClient({
+    defaultOptions: {
+      queries: {
+        staleTime: 60 * 1000,
+      },
+    },
+  });
+}
+
+let broswerQueryClient: QueryClient | undefined = undefined;
+
+function getQueryClient() {
+  if (isServer) {
+    return makeQueryClient();
+  } else {
+    if (!broswerQueryClient) broswerQueryClient = makeQueryClient();
+    return broswerQueryClient;
+  }
+}
 
 export default function Providers({ children }: { children: React.ReactNode }) {
-  const [queryClient] = useState(
-    () =>
-      new QueryClient({
-        defaultOptions: {
-          queries: {
-            staleTime: 60 * 1000,
-          },
-        },
-      })
-  );
-
+  const queryClient = getQueryClient();
   return (
     <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
   );
 }
-
-
